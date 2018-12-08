e11563ea1feb536b16098d29e8c3bb11570097e5
Benjamin Spain <bspain@trackhs.com>
2018-12-08 10:28:59 -0600

    Cleanup

--- client/src/components/NavigationView/NavigationViewRow.tsx
+++ client/src/components/NavigationView/NavigationViewRow.tsx
@@ -14,19 +14,8 @@ class NavigationViewRow extends React.Component<Props, object>
         this.props.onClick(this.props.id);
     }
 
-
-
     render()
     {
-        //const active = (this.props.activeEntry);
-    //     if (this.props.activeEntry) 
-    //     {
-    //         return <p onClick={this.onClickHandler}><b>{this.props.name}</b></p>;
-    //     }
-    //     else
-    //     {
-    //         return <p onClick={this.onClickHandler}>{this.props.name}</p>;
-    //     }
         return <ListGroupItem 
             active={this.props.activeEntry}
             onClick={this.onClickHandler}>{this.props.name}</ListGroupItem>
