9f391740958a7f88fe313a0152b84fb84a9a6cad
Benjamin Spain <bspain@trackhs.com>
2018-12-07 20:16:08 -0600

    Hook up navigation view to store

--- client/src/components/NavigationView/NavigationView.tsx
+++ client/src/components/NavigationView/NavigationView.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import NavigationViewRow from './NavigationViewRow';
+import { IStore, IStoreEntry } from '../../store/Models';
+import { setActiveEntry } from '../../store/Actions';
+import { connect } from 'react-redux';
 
-interface State {
-    activeEntry: string
-    entries: string[]
+
+interface INavigationViewProps
+{
+    entries: Array<{ id: number, name: string }>
+    activeId: number
 }
 
-class NavigationView extends React.Component
+interface INavigationViewActions
 {
-    readonly state: Readonly<State> = 
-    {
-        activeEntry: 'two',
-        entries: [
-            'one', 'two', 'three'
-        ]
-    }
+    setActiveEntry(activeId:number) : void
+}
 
-    onEntryClicked = (entry: string) => {
-        this.setState(stateActiveEntry(this.state, entry));
+class NavigationView extends React.Component<INavigationViewProps & INavigationViewActions>
+{
+    onEntryClicked = (entryId: number) => {
+        this.props.setActiveEntry(entryId);
     }
 
     render()
@@ -25,11 +27,12 @@ class NavigationView extends React.Component
         return (
             <div>
             { 
-                this.state.entries.map(entry => {
+                this.props.entries.map(entry => {
                     return <NavigationViewRow
-                        key={entry} // Not explicitly part of NavigationViewRow props, but React uses this in iterators
-                        entry={entry}
-                        activeEntry={(entry == this.state.activeEntry)}
+                        key={entry.id} // Not explicitly part of NavigationViewRow props, but React uses this in iterators
+                        id={entry.id}
+                        name={entry.name}
+                        activeEntry={(entry.id == this.props.activeId)}
                         onClick={this.onEntryClicked} />
                     })
             }
@@ -38,8 +41,24 @@ class NavigationView extends React.Component
     }
 }
 
-const stateActiveEntry = (prevState: State, newEntry: string) => {
-    return { ...prevState, ...{ activeEntry: newEntry } }
-};
+function mapStateToProps(state: IStore) : INavigationViewProps
+{
+    const navEntries = state.entries.map((storeEntry: IStoreEntry, index: number) => {
+        return { id: index, name: storeEntry.name }
+    });
 
-export default NavigationView
+    return {
+        entries: navEntries,
+        activeId: state.activeEntry
+    }
+}
+
+function mapDispatchToProps(dispatch: any) : INavigationViewActions
+{
+    return {
+        setActiveEntry: (activeId: number) => { dispatch(setActiveEntry(activeId)) }
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationView);
--- client/src/components/NavigationView/NavigationViewRow.tsx
+++ client/src/components/NavigationView/NavigationViewRow.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
 type Props = {
-    entry: string,
+    id: number,
+    name: string,
     activeEntry: boolean,
-    onClick(entry: string) : void
+    onClick(entryId: number) : void
 }
 
 class NavigationViewRow extends React.Component<Props, object>
 {
     onClickHandler = () => {
-        this.props.onClick(this.props.entry);
+        this.props.onClick(this.props.id);
     }
 
     render()
     {
         if (this.props.activeEntry) 
         {
-            return <p onClick={this.onClickHandler}><b>{this.props.entry}</b></p>;
+            return <p onClick={this.onClickHandler}><b>{this.props.name}</b></p>;
         }
         else
         {
-            return <p onClick={this.onClickHandler}>{this.props.entry}</p>;
+            return <p onClick={this.onClickHandler}>{this.props.name}</p>;
         }
     }
 }
