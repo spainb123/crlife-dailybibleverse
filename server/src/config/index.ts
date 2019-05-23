785d6c32fa7aae2c113a19205808c3ba8e032f2b
Benjamin Spain <bspain@trackhs.com>
2019-05-22 20:18:38 -0500

    Remove react from server

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,6 +2,6 @@ import * as express from 'express';
 const app = express();
 
 export default function config() {
-   app.get('/', (req, res) => res.send('CRLife-DBV TS Server'))
+   app.get('/', (req, res) => res.send('CRLife-DBV TS Server!'))
    return app;
 }
