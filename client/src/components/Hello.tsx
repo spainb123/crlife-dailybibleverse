20d4f310518ffc3aafe89c05331d2c3d3c19e158
Benjamin Spain <bspain@trackhs.com>
2019-06-04 19:48:10 -0500

    Watch support for client

--- client/src/components/Hello.tsx
+++ client/src/components/Hello.tsx
@@ -8,6 +8,6 @@ export class Hello extends React.Component<HelloProps, {}> {
     render() {
         const data = JSON.parse(this.props.data);
 
-        return <div><h1>Hello from {this.props.compiler} and {this.props.framework}, now with SSR!</h1><p>Full Date:<b>{data.fullDate}</b></p></div>;
+        return <div><h1>Hello from {this.props.compiler} and {this.props.framework}, now with SSR!!!</h1><p>Full Date:<b>{data.fullDate}</b></p></div>;
     }
 }
