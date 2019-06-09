734ba3a9a5329ccda4558fa38e75871b14b6d745
Benjamin Spain <bspain@trackhs.com>
2019-06-08 20:16:21 -0500

    Abandon css modules

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import IReadingData from '../descriptors/IReadingData';
 
-require('./App.css');
-
 export interface AppProps { data: IReadingData}
 
 export class App extends React.Component<AppProps, {}> {
