26099b9d2f7fd102a81002e7ee332b85b510c122
Benjamin Spain <bspain@trackhs.com>
2019-06-09 16:23:56 -0500

    Scroll to element support

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,11 +3,51 @@ import { Container, Row, Col } from 'reactstrap';
 
 export default class Footer extends React.Component
 {
+    onFooterButtonOTPassClicked = () => {
+        document.getElementById('OTHEAD').scrollIntoView();
+    }
+    onFooterButtonNTPassClicked = () => {
+        document.getElementById('NTHEAD').scrollIntoView();
+    }
+    onFooterButtonPSPassClicked = () => {
+        document.getElementById('PSHEAD').scrollIntoView();
+    }
+    onFooterButtonPRPassClicked = () => {
+        document.getElementById('PRHEAD').scrollIntoView();
+    }
+    onFooterButtonOTNoteClicked = () => {
+        document.getElementById('OTNOTE').scrollIntoView();
+    }
+    onFooterButtonNTNoteClicked = () => {
+        document.getElementById('NTNOTE').scrollIntoView();
+    }
+    onFooterButtonFSNoteClicked = () => {
+        document.getElementById('FSNOTE').scrollIntoView();
+    }
+
     render() {
         return (
             <Container fluid={true} className="Footer">
                 <Row noGutters={true}>
-                    <Col sm={12}>Footer</Col>
+                    <Col sm={6}>Study</Col>
+                    <Col sm={6}>Reading</Col>
+                </Row>
+                <Row noGutters={true}>
+                    <Col sm={6}>
+                        <Row>
+                            <Col sm={4} onClick={this.onFooterButtonOTNoteClicked}>OT</Col>
+                            <Col sm={4} onClick={this.onFooterButtonNTNoteClicked}>NT</Col>
+                            <Col sm={4} onClick={this.onFooterButtonFSNoteClicked}>More</Col>
+                        </Row>
+                    </Col>
+                    <Col sm={6}>
+                        <Row>
+                            <Col sm={3} onClick={this.onFooterButtonOTPassClicked}>OT</Col>
+                            <Col sm={3} onClick={this.onFooterButtonNTPassClicked}>NT</Col>
+                            <Col sm={3} onClick={this.onFooterButtonPSPassClicked}>Psalm</Col>
+                            <Col sm={3} onClick={this.onFooterButtonPRPassClicked}>Proverbs</Col>
+                        </Row>
+                    </Col>
                 </Row>
             </Container>
         )
--- client/src/components/ReadingData.tsx
+++ client/src/components/ReadingData.tsx
@@ -3,25 +3,23 @@ import IReadingData from '../descriptors/IReadingData';
 
 const ReadingData: React.SFC<{ data: IReadingData }> = (props) => (
         <div className='ReadingData'>
-        <div>Full Date: {props.data.fullDate}</div>
-        <h1>Old Testament Notes</h1>
-        <div dangerouslySetInnerHTML={{ __html: props.data.note.ot }}></div>
-        <h1>Old Testament Reading</h1>
-        <h2>{props.data.pass.ot.heading}</h2>
-        <div dangerouslySetInnerHTML={{ __html: props.data.pass.ot.body }}></div>
-        <h1>New Testament Notes</h1>
-        <div dangerouslySetInnerHTML={{ __html: props.data.note.nt }}></div>
-        <h1>New Testament Reading</h1>
-        <h2>{props.data.pass.nt.heading}</h2>
-        <div dangerouslySetInnerHTML={{ __html: props.data.pass.nt.body }}></div>
-        <h1>Psalms Reading</h1>
-        <h2>{props.data.pass.ps.heading}</h2>
-        <div dangerouslySetInnerHTML={{ __html: props.data.pass.ps.body }}></div>
-        <h1>Proverbs Reading</h1>
-        <h2>{props.data.pass.pr.heading}</h2>
-        <div dangerouslySetInnerHTML={{ __html: props.data.pass.pr.body }}></div>
-        <h1>Further Study</h1>
-        <div dangerouslySetInnerHTML={{ __html: props.data.note.fs }}></div>
+                <div>Full Date: {props.data.fullDate}</div>
+                <div id={'OTNOTE'} dangerouslySetInnerHTML={{ __html: props.data.note.ot }}></div>
+                <h1 id={'OTHEAD'}>Old Testament Reading</h1>
+                <h2>{props.data.pass.ot.heading}</h2>
+                <div id={'OTBODY'} dangerouslySetInnerHTML={{ __html: props.data.pass.ot.body }}></div>
+                <div id={'NTNOTE'} dangerouslySetInnerHTML={{ __html: props.data.note.nt }}></div>
+                <h1 id={'NTHEAD'}>New Testament Reading</h1>
+                <h2>{props.data.pass.nt.heading}</h2>
+                <div id={'NTBODY'} dangerouslySetInnerHTML={{ __html: props.data.pass.nt.body }}></div>
+                <h1 id={'PSHEAD'}>Psalms Reading</h1>
+                <h2>{props.data.pass.ps.heading}</h2>
+                <div id={'PSBODY'} dangerouslySetInnerHTML={{ __html: props.data.pass.ps.body }}></div>
+                <h1 id={'PRHEAD'}>Proverbs Reading</h1>
+                <h2>{props.data.pass.pr.heading}</h2>
+                <div id={'PRBODY'} dangerouslySetInnerHTML={{ __html: props.data.pass.pr.body }}></div>
+                <div id={'FSNOTE'} dangerouslySetInnerHTML={{ __html: props.data.note.fs }}></div>
+                <div id={'READBUFFER'}></div>
         </div>
 );
 
