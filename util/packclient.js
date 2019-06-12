390220760485d6bff638552c3979ab80547b0ac8
Benjamin Spain <bspain@trackhs.com>
2019-06-12 17:45:26 -0500

    Favicon

--- util/packclient.js
+++ util/packclient.js
@@ -6,6 +6,7 @@ const ncp = require('ncp').ncp;
 console.log('Starting packclient...');
 
 const client_indexpage = path.join(__dirname, '../client/index.pack.html');
+const client_favicon = path.join(__dirname, '../client/favicon.png');
 const client_dist = path.join(__dirname, '../client/dist');
 const server_public = path.join(__dirname, '../public/');
 
@@ -21,6 +22,9 @@ fs.mkdirSync(server_public);
 
 console.log('Copying index.html');
 fs.copyFileSync(client_indexpage, path.join(server_public, "index.html"));
+
+console.log('Copying favicon.png');
+fs.copyFileSync(client_favicon, path.join(server_public, 'favicon.png'));
 
 console.log('Copying Client dist');
 ncp(client_dist, path.join(server_public, 'dist'), () => {
