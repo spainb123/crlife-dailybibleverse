111fa283f316d44b746b13700c086e8087165da1
Benjamin Spain <benjamin@thespains.net>
2019-09-08 11:34:27 -0500

    Only use whitelisted and masked keys in health

--- src/modules/health/healthService.ts
+++ src/modules/health/healthService.ts
@@ -9,21 +9,45 @@ export default class HealthService implements IModuleRequestHandler
 
     requestHandler(request: import("express-serve-static-core").Request, response: import("express-serve-static-core").Response): void {
 
-        const settings = {...process.env};
-        const maskedKeys = ["_CUSTOM_APP_SETTING", "NLT_API_KEY", "AZURE_STORAGE_ACCOUNT_NAME", "AZURE_STORAGE_ACCOUNT_ACCESS_KEY", "AZURE_STORAGE_CONTAINER_NAME" ]
+        const visibleSettings:any = {};
+
+        // For a list of all keys (and other things) available to apps, see: http://whatazurewebsiteenvironmentvariablesareavailable.azurewebsites.net/
+        const whitelistedKeys = [
+            "DEBUG",
+            "META",
+            "_DAILY_AZURE",
+
+            // Azure App Service specific keys
+            "NODE_VERSION",
+            "YARN_VERSION",
+        ]
+        const maskedKeys = [
+            "_CUSTOM_APP_SETTING", 
+            "NLT_API_KEY", 
+            "AZURE_STORAGE_ACCOUNT_NAME", 
+            "AZURE_STORAGE_ACCOUNT_ACCESS_KEY", 
+            "AZURE_STORAGE_CONTAINER_NAME",
+        ]
+
+        whitelistedKeys.forEach(wk => {
+            if (process.env[wk])
+            {
+                visibleSettings[wk] = process.env[wk];
+            }
+        })
 
         // Mask particular values
         maskedKeys.forEach(mk => {
-            if (settings[mk])
+            if (process.env[mk])
             {
-                settings[mk] = settings[mk].replace(/./g, '#');
+                visibleSettings[mk] = process.env[mk].replace(/./g, '#');
             }    
         })
 
         response.setHeader('Content-Type', 'application/json');
         response.send(JSON.stringify({
             status: "OK",
-            environment: settings
+            environment: visibleSettings
         }));
     }
     
